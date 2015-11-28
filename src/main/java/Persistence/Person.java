aac25bea57f6f6472720ffecaf96f6c41c638de7
ramol1 <luca.ramosbrito@students.bfh.ch>
2015-11-28 14:15:25 +0100

    kleine korrekturen

--- src/main/java/Persistence/Person.java
+++ src/main/java/Persistence/Person.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Inheritance;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
