0ef935e864e5523855bf46ef4b07f2deaec3faf8
sarahmele <sarah.mele@students.bfh.ch>
2015-11-29 15:48:49 +0100

    Home und Login

--- src/main/java/views/LoginView.java
+++ src/main/java/views/LoginView.java
@@ -1,16 +1,13 @@
 package views;
 
 import java.io.File;
-
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
+
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FileResource;
-import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinService;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -19,61 +16,55 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- *
- */
+import ch.bfh.btx8081.w2015.green.doctorGreen.MyUI;
+
+@SuppressWarnings("serial")
 @Theme("mytheme")
 @Widgetset("ch.bfh.btx8081.w2015.green.doctorGreen.MyAppWidgetset")
-public class LoginView extends UI {
-
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        
-        // Find the application directory
-        String basepath = VaadinService.getCurrent()
-                          .getBaseDirectory().getAbsolutePath();
-        
-        System.out.println(basepath);
-
-        // Image as a file resource
-        FileResource resource = new FileResource(new File(basepath +
-                                "/VAADIN/images/doctor_green.png"));
-
+public class LoginView extends VerticalLayout implements View {
+	
+	public LoginView() {
+		setSizeFull();
+		setSpacing(true);
+		
+		// Find the application directory
+		String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+		 // Image as a file resource
+        FileResource resource = new FileResource(new File(basepath + "/VAADIN/images/doctor_green.png"));
         // Show the image in the application
         Image image = new Image("",resource);
         
-        
-        TextField usernameTextfield = new TextField("Username");
-        PasswordField passwordDoctor = new PasswordField("Password");
-       
-        // Put some initial content in it
-        usernameTextfield.setValue("Doctor Green");
-  
-        Button loginButton = new Button("Login");
-        loginButton.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-            	Notification.show("Wrong Login");
-            }
-        });
-        layout.addComponent(image);
-        layout.addComponent(new Label("&nbsp;", ContentMode.HTML));
-        layout.addComponent(usernameTextfield);
-        layout.addComponent(new Label("&nbsp;", ContentMode.HTML));
-        layout.addComponent(passwordDoctor);
-        layout.addComponent(new Label("&nbsp;", ContentMode.HTML));
-        layout.addComponent(loginButton);
+        TextField username = new TextField("Username");
+		PasswordField password = new PasswordField("Password");
+		// Put some initial content in it
+		password.setValue("Doctor Green");
+		
+		addComponent(image);
+		addComponent(new Label("&nbsp;", ContentMode.HTML));
+		addComponent(username);
+		addComponent(new Label("&nbsp;", ContentMode.HTML));
+		addComponent(password);
+		addComponent(new Label("&nbsp;", ContentMode.HTML));
+		addComponent(loginButton());
+	}
+	
+	
+	private Button loginButton() {
+		Button button = new Button("Log In", new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				getUI().getNavigator().navigateTo(MyUI.HOMEVIEW);
+			}
+		});
+		return button;
+	}	
 
-    }
-
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = LoginView.class, productionMode = false)
-    public static class LoginViewServlet extends VaadinServlet {
-    }
+	@Override
+	public void enter(ViewChangeEvent event) {
+		Notification.show("Welcome! Please log in.");
+	}
+	
 }
+	
