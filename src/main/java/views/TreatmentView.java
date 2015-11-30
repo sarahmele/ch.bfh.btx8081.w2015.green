8e58dd3b657e523470ce5e3a5dc0a7ef7c10b5ac
ramol1 <luca.ramosbrito@students.bfh.ch>
2015-11-30 11:49:04 +0100

    erstellung treatmentView

---
+++ src/main/java/views/TreatmentView.java
@@ -0,0 +1,37 @@
+package views;
+
+import java.io.File;
+import java.util.Date;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FileResource;
+import com.vaadin.server.VaadinService;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Button.ClickEvent;
+
+import ch.bfh.btx8081.w2015.green.doctorGreen.MyUI;
+
+import com.vaadin.ui.Calendar;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("mytheme")
+@Widgetset("ch.bfh.btx8081.w2015.green.doctorGreen.MyAppWidgetset")
+public class TreatmentView extends VerticalLayout implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
