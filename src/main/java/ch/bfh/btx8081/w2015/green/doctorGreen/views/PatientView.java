cc9acb13666da8206251509af8630e3bf7b6a184
porty1 <porty1@bfh.ch>
2015-12-03 15:28:27 +0100

    porty1: Created File

---
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/PatientView.java
@@ -0,0 +1,14 @@
+package ch.bfh.btx8081.w2015.green.doctorGreen.views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+public class PatientView implements View {
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
