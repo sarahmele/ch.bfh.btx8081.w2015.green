df4a2657f318e15e6129f90ecddc8f1b4de51c57
sarahmele <sarah.mele@students.bfh.ch>
2015-12-16 21:57:07 +0100

    Home

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/HomeView.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/HomeView.java
@@ -2,6 +2,7 @@ package ch.bfh.btx8081.w2015.green.doctorGreen.views;
 
 import java.io.File;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
@@ -18,11 +19,11 @@ import ch.bfh.btx8081.w2015.green.doctorGreen.MyUI;
 
 import com.vaadin.ui.Calendar;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.calendar.CalendarComponentEvents;
+import com.vaadin.ui.components.calendar.CalendarComponentEvents.EventClick;
+import com.vaadin.ui.components.calendar.event.BasicEvent;
 
 /**
  *
@@ -43,6 +44,41 @@ public class HomeView extends VerticalLayout implements View {
 		Calendar cal = new Calendar("Patient-Calendar");
 		cal.setStartDate(new Date());
 		cal.setEndDate(new Date());
+		cal.setFirstVisibleHourOfDay(7);
+		cal.setLastVisibleHourOfDay(17);
+		
+		//cal.setHandler(new );
+		
+		cal.setHandler(new CalendarComponentEvents.EventClickHandler() {
+			@Override
+		    public void eventClick(EventClick event) {
+		     //   BasicEvent e = (BasicEvent) event.getCalendarEvent();
+
+		        // Do something with it
+		        getUI().getNavigator().navigateTo(MyUI.PATIENTVIEW);
+		        
+		        /*Do something with it
+		        new Notification("Event clicked: " + e.getCaption(),
+		                e.getDescription()).show(Page.getCurrent());*/
+		    }
+		});
+		
+		
+		// Add two events
+		GregorianCalendar start = new GregorianCalendar();
+		GregorianCalendar end   = new GregorianCalendar();
+		start.set(2015, 11, 16, 9, 00);
+		end.set(2015, 11, 16, 11, 00);
+		cal.addEvent(new BasicEvent("John Teller",
+				       "Watch the Case of John Teller",
+				        start.getTime(), end.getTime()));
+		
+		start.set(2015, 11, 14, 13, 00);
+		end.set(2015, 11, 14, 15, 00);
+		cal.addEvent(new BasicEvent("Marlis Müller",
+				       "Watch the Case of Marlis Müller",
+				        start.getTime(), end.getTime()));
+		
 		
 		// Find the application directory
 		String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
@@ -57,6 +93,7 @@ public class HomeView extends VerticalLayout implements View {
 				getUI().getNavigator().navigateTo(MyUI.LOGINVIEW);
 			}
 		});
+		backButton.addStyleName("green");
 		
 		Button homeButton = new Button("Home", new Button.ClickListener() {
 			@Override
@@ -64,37 +101,39 @@ public class HomeView extends VerticalLayout implements View {
 				getUI().getNavigator().navigateTo(MyUI.HOMEVIEW);
 			}
 		});
+		homeButton.addStyleName("green");
 		
-		Button caseView_Button = new Button("Case", new Button.ClickListener() {
+		Button patientView_Button = new Button("Case", new Button.ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
 				getUI().getNavigator().navigateTo(MyUI.CASEVIEW);
 			}
 		});
 		
-		Button patientView_Button = new Button("Patient", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				getUI().getNavigator().navigateTo(MyUI.PATIENTVIEW);
-			}
-		});
-		
 		layoutHeader.addComponent(backButton);
+		layoutHeader.setComponentAlignment(backButton,Alignment.TOP_CENTER);
 		layoutHeader.addComponent(image);
+		layoutHeader.setComponentAlignment(image,Alignment.TOP_CENTER);
+		
 		layoutHeader.addComponent(homeButton);
-		layoutFooter.addComponent(caseView_Button);
+		layoutHeader.setComponentAlignment(homeButton,Alignment.TOP_CENTER);
+		
 		layoutFooter.addComponent(patientView_Button);
 		
 		addComponent(layoutHeader);
 		addComponent(cal);
 		addComponent(layoutFooter);
 		setComponentAlignment(cal,Alignment.MIDDLE_CENTER);
+	
+		
+		
+		
 		
 	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
-		Notification.show("Welcome! This is Home.");
+		//Notification.show("Welcome! This is Home.");
 	}
 	
 
