0ac69ba1a74bc337a0fbdcdb934b854bd2500c95
pfism11 <manuel.pfister@students.bfh.ch>
2015-12-16 19:18:10 +0100

    Parameter auf eingerichtete DB von Hr Pfahrer ergänzt und auf provisorische Tabelle referenziert

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/controller/LogAuthorDB.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/controller/LogAuthorDB.java
@@ -17,8 +17,8 @@ public class LogAuthorDB {
 	public String AuthorResult = null;
 	private Object MyLog;
 	private Object MyPassw;
-	public static final String DATABASE = "I2r2014";
-	
+	//public static final String DATABASE = "I2r2014"; grosser Datensatz aus Übungen
+	public static final String DATABASE = "SWE_2015_1"; //Zugang zu unser SQL-Datenbank Projekt
 
 	public LogAuthorDB() {
 
@@ -31,8 +31,11 @@ public class LogAuthorDB {
 		Connection con = null;
 		Statement statement = null;
 		ResultSet rs = null;
-		String lastName = "";
-		String firstName = "";
+		//String lastName = "";
+		//String firstName = "";
+		String username ="";
+		String passwort ="";
+		
 		this.MyLog =  MyLog;
 		MyLog =  "'"+MyLog+"'";
 		this.MyPassw =  MyPassw;
@@ -49,8 +52,10 @@ public class LogAuthorDB {
 			 * ist, werden entsprechende Parameter angepasst
 			 */
 			con = DriverManager.getConnection(
-					"jdbc:jtds:sqlserver://corpus.bfh.ch:55783;DatabaseName="
-							+ DATABASE, "scott", "tiger");
+					//"jdbc:jtds:sqlserver://corpus.bfh.ch:55783;DatabaseName="
+							//+ DATABASE, "scott", "tiger");
+			"jdbc:jtds:sqlserver://corpus.bfh.ch:55783;DatabaseName="
+			+ DATABASE, "SWE_2015_1", "swe2015");
 
 			System.out.println("Connection to MS SQL DB successful!:");
 
@@ -63,14 +68,22 @@ public class LogAuthorDB {
 			// Parameter Name und FirstName werden später
 			// entsprechend angepasst, PatientNb ist hier überflüssig, warte aber 
 					//noch mit anpassen bis Datenbank fix ist
-					("SELECT PatientNb, Name, FirstName " + " FROM " + DATABASE
+					/*("SELECT PatientNb, Name, FirstName " + " FROM " + DATABASE
 							+ ".dbo.Patient "
 							//+ "where Name ='Abati' and Firstname ='Jan'");
-			+ "where Name ="+MyLog +"and Firstname ="+MyPassw); 
+			+ "where Name ="+MyLog +"and Firstname ="+MyPassw); */
+			
+					
+					("SELECT PatientNb, Username, Passwort " + " FROM " + DATABASE
+							+ ".dbo.TESTPATIENT "
+							//+ "where Username ='wert12' and Passwort ='Jan'");
+			+ "where Username ="+MyLog +"and Passwort ="+MyPassw); 
+					
 			MyLog =  MyLog.replace("'",""); //Der User dürfte beim Loggin somit kein "'" verwenden
 			MyPassw =  MyPassw.replace("'",""); //Der User dürfte beim Loggin somit kein "'" verwenden
 			
-			System.out.println("PatientNb\tName, Firstname");
+			System.out.println("PatientNb\tUsername, Passwort");
+			//System.out.println("PatientNb\tName, Firstname");
 			System.out.println("---------\t---------------");
 			int i = 0;
 
@@ -79,21 +92,28 @@ public class LogAuthorDB {
 				if (i < 1) {
 					System.out.println(String.format("%d\t%d\t%s, %s", i++,
 							rs.getInt(1), rs.getString(2), rs.getString(3)));
-					lastName = rs.getString("Name");
-					System.out.println(lastName);
-					firstName = rs.getString("FirstName");
-					System.out.println(firstName);
+					username = rs.getString("Username");
+					//lastName = rs.getString("Name");
+					System.out.println(username);
+					//System.out.println(lastName);
+					passwort = rs.getString("Passwort");
+					//firstName = rs.getString("FirstName");
+					System.out.println(passwort);
 				}
 
 			// da bei SQL Datenbankabfrage auch "" zurück kommen kann, muss dies 
 			//hier in der if Schleife abgefangen werden
-			if (!(MyLog.equals(""))&&((lastName.compareTo(MyLog) == 0)
-					&& (firstName.compareTo(MyPassw) == 0))) {
+			//if (!(MyLog.equals(""))&&((lastName.compareTo(MyLog) == 0)
+			//		&& (firstName.compareTo(MyPassw) == 0))) {
+			if (!(MyLog.equals(""))&&((username.compareTo(MyLog) == 0)
+					&& (passwort.compareTo(MyPassw) == 0))) {
 				AuthorResult = "true";
 				System.out.println("Erfolgreiches Login");
-				System.out.println("Dies wird aus der DB ausgelesen: " + lastName);
+				//System.out.println("Dies wird aus der DB ausgelesen: " + lastName);
+				System.out.println("Dies wird aus der DB ausgelesen: " + username);
 				System.out.println("Dies wird aus der Tastatur ausgelesen: " + MyLog);
-				System.out.println("Dies wird aus der DB ausgelesen: " + firstName);
+				//System.out.println("Dies wird aus der DB ausgelesen: " + firstName);
+				System.out.println("Dies wird aus der DB ausgelesen: " + passwort);
 				System.out.println("Dies wird aus der Tastatur ausgelesen: " + MyPassw);
 				
 			}
@@ -101,9 +121,11 @@ public class LogAuthorDB {
 			else {
 				AuthorResult = "false";
 				System.out.println("Login fehlgeschlagen");
-				System.out.println("Dies wird aus der DB ausgelesen: " + lastName);
+				//System.out.println("Dies wird aus der DB ausgelesen: " + lastName);
+				System.out.println("Dies wird aus der DB ausgelesen: " + username);
 				System.out.println("Dies wird aus der Tastatur ausgelesen: " + MyLog);
-				System.out.println("Dies wird aus der DB ausgelesen: " + firstName);
+				//System.out.println("Dies wird aus der DB ausgelesen: " + firstName);
+				System.out.println("Dies wird aus der DB ausgelesen: " + passwort);
 				System.out.println("Dies wird aus der Tastatur ausgelesen: " + MyPassw);
 
 			}
