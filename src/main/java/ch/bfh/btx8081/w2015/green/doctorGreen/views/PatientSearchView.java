8c9502709963e4f77f888146116c6f4c342181d5
sarahmele <sarah.mele@students.bfh.ch>
2015-12-17 09:57:04 +0100

    PatientSearchView as a MyUI

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/PatientSearchView.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/PatientSearchView.java
@@ -1,9 +1,15 @@
 package ch.bfh.btx8081.w2015.green.doctorGreen.views;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
 
-public class PatientSearchView implements View {
+@SuppressWarnings("serial")
+@Theme("mytheme")
+@Widgetset("ch.bfh.btx8081.w2015.green.doctorGreen.MyAppWidgetset")
+public class PatientSearchView extends VerticalLayout implements View {
 
 	@Override
 	public void enter(ViewChangeEvent event) {
