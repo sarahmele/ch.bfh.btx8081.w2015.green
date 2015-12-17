861dfb0d41a7a3d81ce933e71bda6b162a15afdd
porty1 <porty1@bfh.ch>
2015-12-17 10:28:49 +0100

    porty1: Relation Status changed

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/persistence/Patient.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/persistence/Patient.java
@@ -8,12 +8,16 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Patient extends Person {
 
 	@Column
 	private String insuranceNb;
+	
+	@OneToOne
+	private Status status;
 
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "DOCTOR_PATIENT", joinColumns = @JoinColumn(name = "DOCTOR_ID", referencedColumnName = "pid") , inverseJoinColumns = @JoinColumn(name = "PATIENT_ID", referencedColumnName = "pid") )
@@ -22,5 +26,29 @@ public class Patient extends Person {
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "PATIENT_CASE", joinColumns = @JoinColumn(name = "PATIENT_ID", referencedColumnName = "pid") , inverseJoinColumns = @JoinColumn(name = "PATIENT_CASE_ID", referencedColumnName = "patientCaseId") )
 	private List<PatientCase> patientCaseList;
+
+	public String getInsuranceNb() {
+		return insuranceNb;
+	}
+
+	public void setInsuranceNb(String insuranceNb) {
+		this.insuranceNb = insuranceNb;
+	}
+
+	public List<Doctor> getDoctorList() {
+		return doctorList;
+	}
+
+	public void setDoctorList(List<Doctor> doctorList) {
+		this.doctorList = doctorList;
+	}
+
+	public List<PatientCase> getPatientCaseList() {
+		return patientCaseList;
+	}
+
+	public void setPatientCaseList(List<PatientCase> patientCaseList) {
+		this.patientCaseList = patientCaseList;
+	}
 
 }
