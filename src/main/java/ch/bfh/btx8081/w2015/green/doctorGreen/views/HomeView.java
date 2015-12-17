3cd9503eef5121605f529feb829bb03c8fac99cb
sarahmele <sarah.mele@students.bfh.ch>
2015-12-17 09:56:33 +0100

    Home View with Generic Header

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/HomeView.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/HomeView.java
@@ -1,6 +1,5 @@
 package ch.bfh.btx8081.w2015.green.doctorGreen.views;
 
-import java.io.File;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -8,18 +7,15 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.FileResource;
-import com.vaadin.server.VaadinService;
-import com.vaadin.ui.Alignment;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Button.ClickEvent;
 
 import ch.bfh.btx8081.w2015.green.doctorGreen.MyUI;
 
 import com.vaadin.ui.Calendar;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.components.calendar.CalendarComponentEvents;
 import com.vaadin.ui.components.calendar.CalendarComponentEvents.EventClick;
@@ -34,101 +30,85 @@ import com.vaadin.ui.components.calendar.event.BasicEvent;
 public class HomeView extends VerticalLayout implements View {
 	
 	public HomeView() {
-		setSizeFull();
-		setSpacing(true);
 		
-		// layout for header and footer
-		HorizontalLayout layoutHeader = new HorizontalLayout();
+		// layout for footer
 		HorizontalLayout layoutFooter = new HorizontalLayout();
 
-		Calendar cal = new Calendar("Patient-Calendar");
+		// Make a new Calendar - Date Today - Hours of the Day seen 7.00am - 5.00pm
+		Calendar cal = new Calendar();
 		cal.setStartDate(new Date());
 		cal.setEndDate(new Date());
 		cal.setFirstVisibleHourOfDay(7);
 		cal.setLastVisibleHourOfDay(17);
 		
-		//cal.setHandler(new );
-		
+		//Set EventHandler to the Calendar - if you Click on an Event, a new View is Opened
 		cal.setHandler(new CalendarComponentEvents.EventClickHandler() {
 			@Override
 		    public void eventClick(EventClick event) {
-		     //   BasicEvent e = (BasicEvent) event.getCalendarEvent();
-
-		        // Do something with it
 		        getUI().getNavigator().navigateTo(MyUI.PATIENTVIEW);
-		        
-		        /*Do something with it
-		        new Notification("Event clicked: " + e.getCaption(),
-		                e.getDescription()).show(Page.getCurrent());*/
 		    }
 		});
 		
 		
-		// Add two events
+		// Add two events for today
 		GregorianCalendar start = new GregorianCalendar();
 		GregorianCalendar end   = new GregorianCalendar();
-		start.set(2015, 11, 16, 9, 00);
-		end.set(2015, 11, 16, 11, 00);
+		start.set(2015, 11, 17, 9, 00);
+		end.set(2015, 11, 17, 11, 00);
 		cal.addEvent(new BasicEvent("John Teller",
 				       "Watch the Case of John Teller",
 				        start.getTime(), end.getTime()));
 		
-		start.set(2015, 11, 14, 13, 00);
-		end.set(2015, 11, 14, 15, 00);
+		start.set(2015, 11, 17, 13, 00);
+		end.set(2015, 11, 17, 15, 00);
 		cal.addEvent(new BasicEvent("Marlis Müller",
 				       "Watch the Case of Marlis Müller",
 				        start.getTime(), end.getTime()));
 		
-		
-		// Find the application directory
-		String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-		// Image as a file resource
-		FileResource resource = new FileResource(new File(basepath + "/VAADIN/images/doctor_green_small.png"));
-		// Show the image in the application
-		Image image = new Image("",resource);
-		
-		Button backButton = new Button("Back", new Button.ClickListener() {
+		//Button to enter SearchView
+		Button patientSearchButton = new Button("Search", new Button.ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
-				getUI().getNavigator().navigateTo(MyUI.LOGINVIEW);
+				getUI().getNavigator().navigateTo(MyUI.PATIENTSEARCHVIEW);
 			}
 		});
-		backButton.addStyleName("green");
+		patientSearchButton.addStyleName("green");
 		
-		Button homeButton = new Button("Home", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				getUI().getNavigator().navigateTo(MyUI.HOMEVIEW);
-			}
-		});
-		homeButton.addStyleName("green");
-		
-		Button patientView_Button = new Button("Case", new Button.ClickListener() {
+		//Button for Vladi to watch the PatientView --> IMPORTANT: DELETE, WHEN IMPLEMENTED
+		Button patientViewButton = new Button("Case", new Button.ClickListener() {
 			@Override
 			public void buttonClick(ClickEvent event) {
 				getUI().getNavigator().navigateTo(MyUI.CASEVIEW);
 			}
 		});
+		patientViewButton.addStyleName("green");
 		
-		layoutHeader.addComponent(backButton);
-		layoutHeader.setComponentAlignment(backButton,Alignment.TOP_CENTER);
-		layoutHeader.addComponent(image);
-		layoutHeader.setComponentAlignment(image,Alignment.TOP_CENTER);
 		
-		layoutHeader.addComponent(homeButton);
-		layoutHeader.setComponentAlignment(homeButton,Alignment.TOP_CENTER);
+		// make the Footer
+		layoutFooter.addComponent(patientViewButton);
+		layoutFooter.addComponent(new Label("&nbsp;", ContentMode.HTML));
+		layoutFooter.addComponent(patientSearchButton);
 		
-		layoutFooter.addComponent(patientView_Button);
+		// get the Header
+		GenericView header = new GenericView();
+		HorizontalLayout layoutHeader = header.getHeader();
 		
+		// Back Button is Logout Button in this View
+		header.getButton("back").setCaption("Logout");
+		header.getButton("back").addClickListener(new Button.ClickListener() {
+			@Override
+		    public void buttonClick(ClickEvent event) {
+		        getUI().getNavigator().navigateTo(MyUI.LOGINVIEW);
+		    }
+		});
+		
+		// no home Button in this View
+		header.getButton("home").setVisible(false);
+		
+		// add all components to the Layout
 		addComponent(layoutHeader);
 		addComponent(cal);
 		addComponent(layoutFooter);
-		setComponentAlignment(cal,Alignment.MIDDLE_CENTER);
-	
-		
-		
-		
-		
 	}
 
 	@Override
