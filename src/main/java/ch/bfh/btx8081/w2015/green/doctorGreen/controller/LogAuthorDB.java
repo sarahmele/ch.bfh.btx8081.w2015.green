c842f151048ce88f7b7c22ba832728bd987dfa84
pfism11 <manuel.pfister@students.bfh.ch>
2016-01-17 19:24:30 +0100

    made code workable

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/controller/LogAuthorDB.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/controller/LogAuthorDB.java
@@ -98,8 +98,8 @@ public class LogAuthorDB {
 					//System.out.println(String.format("%s\t\t%s, \t%s", i++,
 					//		rs.getString(2), rs.getString(3)));
 							
-					System.out.println(String.format("%s\t%s, %s", i++,
-							 rs.getString(6), rs.getString(8)));
+					//System.out.println(String.format("\t%s, %s", i++,
+					//		 rs.getString(6), rs.getString(8)));
 					
 
 					//username = rs.getString("Username"); // Konzeptioneller
