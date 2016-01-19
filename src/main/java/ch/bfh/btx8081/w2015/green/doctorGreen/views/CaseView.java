1d240303e835f9de07dc2f1a83a66bfa6221c27c
Luca <luca.ramosbrito@students.bfh.ch>
2016-01-19 18:05:52 +0100

    Added some changes to the treatment tab

--- src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/CaseView.java
+++ src/main/java/ch/bfh/btx8081/w2015/green/doctorGreen/views/CaseView.java
@@ -2,18 +2,20 @@ package ch.bfh.btx8081.w2015.green.doctorGreen.views;
 
 //import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import ch.bfh.btx8081.w2015.green.doctorGreen.MyUI;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.Table;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -109,75 +111,71 @@ public class CaseView extends VerticalLayout implements View {
 		/* Start of Treatment View Tab code segment */
 		//////////////////////////////////////////////
 
-		Table treatmentTable = new Table("Treatment");
-		treatmentTable.setWidth("320");
-		
-
-		// Define two columns for the built-in container
-		treatmentTable.addContainerProperty("Date", DateField.class, null);
-		treatmentTable.addContainerProperty("Medication", TextArea.class, null);
-
-		/* Add a few items in the table. */
-
-		// Create the fields for the current table row
-		DateField medicationDate = new DateField();
-		medicationDate.setWidth("50%");
-		medicationDate.setValue(new Date());
-		TextArea medicationField = new TextArea();
-		medicationField.setWidth("100%");
-		medicationField.setRows(3);
+		/**
+		 * Defines the datefield that marks the start of a treatment
+		 */
+		DateField treatDate = new DateField("Date");
+		treatDate.setValue(new Date());
+		treatDate.setWidth("80%");
 
 		/**
-		 * Style change variables
+		 * Defines the textarea where the treatment plan is written down
 		 */
-		medicationDate.addStyleName("dateField");
-		medicationField.addStyleName("medicationField");
-		
-		// The Table item identifier for the row.
-		Integer itemId = new Integer(1);
+		TextArea medicationPlan = new TextArea("Medication Plan");
+		medicationPlan.setWidth("100%");
+		medicationPlan.setRows(3);
 
-		// Create the table row.
-		treatmentTable.addItem(
-				new Object[] { medicationDate, medicationField }, itemId);
+		/**
+		 * Defines the button that saves the treatmentplan in the database
+		 */
+		Button saveTreatButton = new Button("Save Treatment");
 
-		// Show exactly the currently contained rows (items)
-		treatmentTable.setPageLength(treatmentTable.size());
+		/**
+		 * Defines the button that is supposed to add additional datefields and textareas
+		 */
+		Button newTreatButton = new Button("New Treatment");
+
+		HorizontalLayout infoBox = new HorizontalLayout();
+		infoBox.setWidth("320px");
+		infoBox.addComponent(treatDate);
+		infoBox.addComponent(medicationPlan);
+
 		
-        
+		HorizontalLayout buttonBox = new HorizontalLayout();
+		buttonBox.setWidth("320px");
+		buttonBox.addComponent(saveTreatButton);
+		buttonBox.addComponent(newTreatButton);
+		
+
+		VerticalLayout treatTab = new VerticalLayout();
+		treatTab.addComponent(infoBox);
+		treatTab.addComponent(buttonBox);
+		//treatTab.addComponent(saveTreatButton);
+		//treatTab.addComponent(newTreatButton);
+
+		/**
+		 * Clicking the "New Treatment" button triggers the event to show 
+		 * another DateField and TextArea
+		 */
+		newTreatButton.addClickListener(new ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent event) {
+				
+			}
+		});
+
 		caseViewTabs.addTab(caseTab).setCaption("Case");
-		caseViewTabs.addComponent(treatmentTable);
-		
-		
+		caseViewTabs.addTab(treatTab).setCaption("Treatment");
+
 		GenericView header = new GenericView();
 		HorizontalLayout layoutHeader = header.getHeader();
-		
+
 		addComponent(layoutHeader);
 		addComponent(caseViewTabs);
-		
-		
 
-		// Treatment Plan button
-		// Button addTreatmentPlan = addTreatmentPlan();
-		// hsplit.setSecondComponent(addTreatmentPlan);
-
-		/* Method to add new Treatment rows */
-		/*
-		 * private void createNewTreatment() { //treatmentTable.addItem(); }
-		 */
-
-		/* Button to add new treatment plans */
-
-		/*
-		 * private Button addTreatmentPlan() { Button button = new
-		 * Button("Add Treatment");
-		 * 
-		 * button.addClickListener(new Button.ClickListener() { public void
-		 * buttonClick(ClickEvent event) { createNewTreatment(); }
-		 * 
-		 * }); return button; }
-		 */
-
+		////////////////////////////////////////////
 		/* End of Treatment View Tab code segment */
+		////////////////////////////////////////////
 
 		// Back Button is Logout Button in this View
 		header.getButton("back").addClickListener(new Button.ClickListener() {
